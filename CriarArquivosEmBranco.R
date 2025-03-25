67250b5a4ec6cd508a14405de6b27df35b3e96ed
fsbmat-ufv <fernando.bastos@ufv.br>
2025-03-25 12:37:26 -0300

    organizacao

--- CriarArquivosEmBranco.R
+++ CriarArquivosEmBranco.R
@@ -1,9 +1,9 @@
 # Define o diretório onde os arquivos serão criados
-pasta_destino <- "~/GitHub/RExams/PR1"  # Altere para o caminho correto
+pasta_destino <- "~/GitHub/RExams/PR1/Cap2"  # Altere para o caminho correto
 
 # Cria os arquivos Pop1.Rmd, Pop2.Rmd, ..., Pop10.Rmd
 for (i in 1:10) {
-        arquivo <- file.path(pasta_destino, paste0("Cap1_2", i, ".Rmd"))
+        arquivo <- file.path(pasta_destino, paste0("Cap2_2", i, ".Rmd"))
         file.create(arquivo)
 }
 
--- geradorxml.R
+++ geradorxml.R
@@ -1,10 +1,16 @@
-setwd("~/GitHub/RExams/PR1")
+setwd("~/GitHub/RExams/PR1/Cap2")
 dir()
 require(exams)
 require(magrittr)
 require(knitr)
 
-exams::exams2moodle(file = "Cap1_29.Rmd", 
+exams::exams2moodle(file = "Cap2_24.Rmd", 
                     n=2, 
                     verbose = TRUE, 
-                    name = "Cap1_29")
+                    name = "Cap2_24", 
+                    svg = FALSE,          # Importante: usa PNG ao invés de SVG
+                    encoding = "UTF-8")
+
+
+exams2html("Cap2_24.Rmd", n = 1)
+
